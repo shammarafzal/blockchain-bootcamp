627c743cb5caf43b32c5818ce2923b9ac777d4e9
Ammar Afzal <ammarafzal075@gmail.com>
2022-04-06 01:51:29 +0500

    Transfer

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,13 +1,19 @@
+const { default: Web3 } = require("web3");
+
 const Token = artifacts.require("./Token");
 
 require("chai").use(require("chai-as-promised")).should();
 
-contract("Token", (accounts) => {
+const tokens = (n) => {
+  return new web3.utils.BN(web3.utils.toWei(n.toString(), "ether"));
+};
+
+contract("Token", ([deployer, receiver]) => {
   let token;
   const name = "ARUM";
   const symbol = "ARU";
   const decimals = "18";
-  const totalSupply = "1000000000000000000000000";
+  const totalSupply = tokens(1000000).toString();
   beforeEach(async () => {
     token = await Token.new();
   });
@@ -29,7 +35,30 @@ contract("Token", (accounts) => {
     });
     it("tracks the total supply", async () => {
       const result = await token.totalSupply();
-      result.toString().should.equal(totalSupply);
+      result.toString().should.equal(totalSupply.toString());
+    });
+    it("assigns the total supply to the deployer", async () => {
+      const result = await token.balanceOf(deployer);
+      result.toString().should.equal(totalSupply.toString());
+    });
+  });
+
+  describe("sending tokens", () => {
+    it("transfers token balances", async () => {
+      let balanceOf;
+      // Before Transfer
+      balanceOf = await token.balanceOf(deployer);
+      console.log("deployer balance before transfer", balanceOf.toString());
+      balanceOf = await token.balanceOf(receiver);
+      console.log("receiver balance before transfer", balanceOf.toString());
+      // After Transfer
+      await token.transfer(receiver, tokens(100), {
+        from: deployer,
+      });
+      balanceOf = await token.balanceOf(deployer);
+      console.log("deployer balance after transfer", balanceOf.toString());
+      balanceOf = await token.balanceOf(receiver);
+      console.log("receiver balance after transfer", balanceOf.toString());
     });
   });
 });
