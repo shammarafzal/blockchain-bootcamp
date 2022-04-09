7ac884ddb06615decc7dbbea8d28d1f2081a52cb
Ammar Afzal <ammarafzal075@gmail.com>
2022-04-10 03:39:50 +0500

    Transfer function

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,12 +5,21 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Token {
     using SafeMath for uint256;
 
+    // Variables
     string public name = "ARUM";
     string public symbol = "ARU";
     uint256 public decimals = 18;
     uint256 public totalSupply;
-
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor() public {
         totalSupply = 1000000 * (10**decimals);
@@ -21,8 +30,40 @@ contract Token {
         public
         returns (bool success)
     {
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        require(balanceOf[msg.sender] >= _value);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+        balanceOf[_from] = balanceOf[_from].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+    }
+
+    // Approve Tokens
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // transfer from
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         return true;
     }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,14 +1,11 @@
 const { default: Web3 } = require("web3");
+import { tokens, EVM_REVERT } from "../src/helpers";
 
 const Token = artifacts.require("./Token");
 
 require("chai").use(require("chai-as-promised")).should();
 
-const tokens = (n) => {
-  return new web3.utils.BN(web3.utils.toWei(n.toString(), "ether"));
-};
-
-contract("Token", ([deployer, receiver]) => {
+contract("Token", ([deployer, receiver, exchange]) => {
   let token;
   const name = "ARUM";
   const symbol = "ARU";
@@ -44,21 +41,84 @@ contract("Token", ([deployer, receiver]) => {
   });
 
   describe("sending tokens", () => {
-    it("transfers token balances", async () => {
-      let balanceOf;
-      // Before Transfer
-      balanceOf = await token.balanceOf(deployer);
-      console.log("deployer balance before transfer", balanceOf.toString());
-      balanceOf = await token.balanceOf(receiver);
-      console.log("receiver balance before transfer", balanceOf.toString());
-      // After Transfer
-      await token.transfer(receiver, tokens(100), {
-        from: deployer,
+    let amount;
+    let result;
+
+    describe("success", async () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        result = await token.transfer(receiver, amount, {
+          from: deployer,
+        });
       });
-      balanceOf = await token.balanceOf(deployer);
-      console.log("deployer balance after transfer", balanceOf.toString());
-      balanceOf = await token.balanceOf(receiver);
-      console.log("receiver balance after transfer", balanceOf.toString());
+
+      it("transfers token balances", async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(tokens(100).toString());
+      });
+
+      it("emits a Transfer event", async () => {
+        const log = result.logs[0];
+        log.event.should.equal("Transfer");
+        const event = log.args;
+        event.from.toString().should.eq(deployer, "from is correct");
+        event.to.should.eq(receiver, "to is correct");
+        event.value.toString().should.eq(amount.toString(), "value is correct");
+      });
+    });
+
+    describe("failure", async () => {
+      it("rejects insufficient balances", async () => {
+        let invalidAmount;
+        invalidAmount = tokens(100000000);
+        await token
+          .transfer(receiver, invalidAmount, { from: deployer })
+          .should.be.rejectedWith(EVM_REVERT);
+
+        invalidAmount = tokens(10);
+        await token
+          .transfer(deployer, invalidAmount, { from: receiver })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+
+      it("rejects invalid recepients", async () => {
+        await token.transfer(0x0, amount, { from: deployer }).should.be
+          .rejected;
+      });
+    });
+  });
+
+  describe("approving tokens", () => {
+    let result;
+    let amount;
+
+    beforeEach(async () => {
+      amount = tokens(100);
+      result = await token.approve(exchange, amount, { from: deployer });
+    });
+
+    describe("success", () => {
+      it("allocates an allowance for delegated token spending on exchange", async () => {
+        const allowance = await token.allowance(deployer, exchange);
+        allowance.toString().should.equal(amount.toString());
+      });
+
+      it("emits an Approval event", async () => {
+        const log = result.logs[0];
+        log.event.should.equal("Approval");
+        const event = log.args;
+        event.owner.toString().should.eq(deployer, "owner is correct");
+        event.spender.should.eq(exchange, "spender is correct");
+        event.value.toString().should.eq(amount.toString(), "value is correct");
+      });
+    });
+    describe("failure", () => {
+      it("rejects invalid spenders", async () => {
+        await token.approve(0x0, amount, { from: deployer }).should.be.rejected;
+      });
     });
   });
 });
